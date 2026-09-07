0875050a1752b49b72d8494fcc0f35cde2429caa
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2026-09-07 20:44:54 +0000

    Make renaming a bit more clear

--- sugar/rename.ml
+++ sugar/rename.ml
@@ -1,38 +1,40 @@
 open SugarAux
 
-(* Source names use a single [split_char]; renamer-issued names carry a doubled
-   one ([tag_sep]), so the two never collide and [is_tagged] can tell them apart. *)
-let split_char = '_'
-let tag_sep = spf "%c%c" split_char split_char
+(* A single [_] is ordinary in a source name; a doubled one marks a tag. *)
+let tag_sep = "__"
 let counter = ref 0
-
-let gensym () =
-  let n = !counter in
-  incr counter;
-  n
 
 let root_of name =
   let n = String.length name and s = String.length tag_sep in
-  let rec last_tag i =
-    if i < 0 then None
-    else if name.[i] = split_char && name.[i + 1] = split_char then Some i
-    else last_tag (i - 1)
+  let rec digits i =
+    if i > 0 && name.[i - 1] >= '0' && name.[i - 1] <= '9' then digits (i - 1)
+    else i
   in
-  match last_tag (n - s) with
-  | Some i ->
-      let tail = String.sub name (i + s) (n - i - s) in
-      if
-        tail <> ""
-        && String.for_all (function '0' .. '9' -> true | _ -> false) tail
-      then String.sub name 0 i
-      else name
-  | None -> name
+  let d = digits n in
+  if d < n && d >= s && String.sub name (d - s) s = tag_sep then
+    String.sub name 0 (d - s)
+  else name
 
 let is_tagged name = root_of name <> name
 
-(* strip any existing tag before re-tagging, so [unique (unique x)] stays [x__n],
-   not [x__n__m] *)
-let unique name = spf "%s%s%i" (root_of name) tag_sep (gensym ())
+let unique name =
+  let n = !counter in
+  incr counter;
+  spf "%s%s%i" (root_of name) tag_sep n
+
 let dummy_var () = unique "dummyVar"
 let fresh_type_var () = unique "tv"
 let fresh_var () = unique "tmp"
+
+(* Misreading a source [<root>_<int>] as tagged is a silent failure. *)
+let%test "root_of strips the renamer's tag and nothing else" =
+  List.map root_of [ "res_0"; "x__7"; "a__b"; "7"; "x__0__12" ]
+  = [ "res_0"; "x"; "a__b"; "7"; "x__0" ]
+
+let%test "unique issues a name it has not issued before" =
+  let a = unique "x" in
+  let b = unique "x" in
+  a <> "x" && b <> "x" && a <> b
+
+let%test "unique re-tags rather than nesting tags" =
+  root_of (unique (unique "x")) = "x"
